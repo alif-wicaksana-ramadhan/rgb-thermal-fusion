d6607ee8e6ff495e946d4bf8bdf7d1dc0deb4060
alifwr <alifwr98@gmail.com>
2024-08-31 15:07:41 +0700

    update train

--- train_yolov10.py
+++ train_yolov10.py
@@ -1,15 +1,23 @@
 from ultralytics import YOLO
 
 # Load YOLOv10n model from scratch
-model = YOLO("yolov10x.yaml").load("yolov10x.pt")
+# model = YOLO("yolov10x.yaml").load("yolov10x.pt")
+model = YOLO("yolov10n.pt")
 
 
 if __name__ == "__main__":
     # Train the model
-    results = model.train(data="datasets/visible.yaml", epochs=100)
+    results = model.train(
+        data="datasets/visible/visible.yaml",
+        epochs=100,
+        imgsz=640,
+        batch=-1,
+        # device="cpu",
+        # workers=0,
+    )
 
     # Save the model
-    model.save("yolov10x_visible.pt")
+    model.save("yolov10n_visible.pt")
 
     # Print the results
-    print(results)
+    # print(results)
